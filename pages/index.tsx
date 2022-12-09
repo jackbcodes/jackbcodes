458fdfcdeff8e04e06fef84f5b0cb81d54d82aeb
Jack Brown <brown_jack80@hotmail.com>
2022-12-09 16:31:00 +0000

    Copy update

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,8 +23,8 @@ export default function Index({ allPosts }: Props) {
           <div className='mx-auto max-w-screen-sm text-center lg:mb-20 mb-8'>
             <h2 className='mb-4 text-3xl lg:text-4xl font-extrabold text-gray-900 dark:text-white'>Hey, I'm Jack</h2>
             <p className='font-light text-gray-500 sm:text-xl dark:text-gray-400'>
-              A software developer who enjoys building quality products and helping others do the same. Subscribe to my
-              newsletter to follow along.
+              A software developer who's all about building quality products and helping others do the same. If you want
+              to stay in the loop, hit that subscribe button for my newsletter.
             </p>
             <SubscribeInput />
           </div>
